e4be2e7f26713cf2c41cca0086ac88d07d93ce79
Ziad <zayad>
2021-01-09 00:35:01 +0300

    added confirmation button

--- SimpleGUI_Layout.py
+++ SimpleGUI_Layout.py
@@ -14,21 +14,29 @@ PDF_List = []
 def MakeMainWindow():
     layout = [[sg.Text('PDF Merger'), sg.Text('      ', k='-OUTPUT-')],
               [sg.T('PDFs')],
-              [sg.Listbox(values=PDF_List, size=(20, 12), key='-LIST-', enable_events=True)],
-              [sg.Button('Add PDF'), sg.Button('Merge'), sg.Button('Exit')]]
+              [sg.Listbox(values=PDF_List, size=(100, 12), key='-LIST-', enable_events=True)],
+              [sg.FileBrowse('Add PDF', key="-IN-", change_submits=True), sg.Button("Confirm File")],
+              [sg.Button("Remove PDF"), sg.Button('Merge'), sg.Button('Exit')]]
 
-    return sg.Window('Window Title', layout, location=(800,600), finalize=True)
-
-
+    return sg.Window('Window Title', layout, location=(800,600), finalize=True,
+                     size = (400,400))
 
 
 window = MakeMainWindow()
 
 while True:             # Event Loop
-    window, event, values = sg.read_all_windows()
+    event, values = window.read()
+
     if event == sg.WIN_CLOSED or event == 'Exit':
         break
-    elif event == 'Add PDF':
-        PDF_List.append("added pdf")
+    elif event  == 'Remove PDF':
+        PDF_List.pop(-1)
         window.Element('-LIST-').Update(values=PDF_List)
+    elif event == 'Confirm File':
+        PDF_List.append(values["-IN-"])
+        window.Element('-LIST-').Update(values=PDF_List)
+    elif event == "Merge":
+        print("Merging PDFS")
+
+
 window.close()
